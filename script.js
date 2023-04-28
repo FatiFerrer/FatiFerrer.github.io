88eaa8e77f660c221e13eeb28228ebe0aa654b7e
FatiFerrer <131365164+FatiFerrer@users.noreply.github.com>
2023-04-28 19:49:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let numero = 1;
+let numero1 = 2;
+let numero2 = 3;
+let mayor;
+
+addEventListener('click', () => {
+if(numero > numero1){
+    mayor = mumero1
+}else(numero1 > numero2);{
+    mayor = numero2
+}
+alert("El mayor numero es el " + mayor)
+})
