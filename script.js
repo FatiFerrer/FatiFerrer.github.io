78f7d38444d222647721683da74da5637b9df7ce
FatiFerrer <131365164+FatiFerrer@users.noreply.github.com>
2023-05-05 19:59:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// primer elercicio, muestra en consola cual es el mayor de determinados numeros 
+
 let numero = 1;
 let numero1 = 2;
 let numero2 = 3;
@@ -13,20 +15,20 @@ if(numero > numero1){
 alert("El mayor numero es el " + mayor)
 })
 
-// segundo ejercicio
+// segundo ejercicio, muestra un mensaje llagado determinado tiempo
 
 setTimeout(function() {
     alert('Hola, han transcurrido 5 segundos desde que ingresaste a esta pagina');
   }, 5000);
   
 
-// contador
+// contador, cuenta del 0 al 10 
 
 for (let i = 0; i < 10; i++) {
     console.log(i);
   }
 
-  //enumerador
+  //ejercio de arrays,
 
 let nombresRandom = ["FATIMA", "ARACELI", "SANDRA", "NOENI", "IHARA", "ARAMI",
  "ANNA", "ERIKA", "MARCELA", "MONICA", "MARIA", "ANDRA", "DAMARIS", "JENNIFER", "LUZ"];
