a3087f57c16f72394566169673e993d2f43b3766
FatiFerrer <131365164+FatiFerrer@users.noreply.github.com>
2023-05-05 19:47:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,41 @@ let numero1 = 2;
 let numero2 = 3;
 let mayor;
 
+
 addEventListener('click', () => {
 if(numero > numero1){
-    mayor = mumero1
+    mayor = numero1
 }else(numero1 > numero2);{
     mayor = numero2
 }
 alert("El mayor numero es el " + mayor)
 })
+
+// segundo ejercicio
+
+setTimeout(function() {
+    alert('Hola, han transcurrido 5 segundos desde que ingresaste a esta pagina');
+  }, 5000);
+  
+
+// contador
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+
+  //enumerador
+
+let nombresRandom = ["FATIMA", "ARACELI", "SANDRA", "NOENI", "IHARA", "ARAMI",
+ "ANNA", "ERIKA", "MARCELA", "MONICA", "MARIA", "ANDRA", "DAMARIS", "JENNIFER", "LUZ"];
+ for (let item of nombresRandom){
+   // console.log(item)
+  }
+ for (let i= 3 ; i < nombresRandom.length; i++) {
+    console.log(nombresRandom [i]);
+    
+    
+}
+ 
+
+
